467c388b96148ae5ae5faad69f356d17936f2aee
Garrett Tjarks <chicarksey@gmail.com>
2014-01-25 18:38:05 -0500

    I broke everything to get the player to move in a full screen house

--- src/main/pixi/js/levels.js
+++ src/main/pixi/js/levels.js
@@ -1,25 +1,29 @@
-function loadMainstreet(background, foreground, onScreenObjects) {
-	emptyScreenObjects(background, foreground, onScreenObjects);
+function loadMainstreet(onScreenObjects, from) {
+	emptyScreenObjects(onScreenObjects);
+	WORLD.AGENT.setX(WORLD.WIDTH / 2);
+	WORLD.MOVEMENT = "world";
 
-	var house = createProp(foreground, 270, "house", 250, 0.3);
+	var house = createProp(WORLD.GROUNDS.foreground, 270, "house", 250, 0.3);
 	house.interact = function() {
-		loadHouse(background, foreground, onScreenObjects);
+		loadHouse(onScreenObjects);
 //		dialog("Go home?",
 //				[
-//				 { 'text': "Yes.", 'response': "Welcome home.", 'action': function() { loadHouse(background, foreground, onScreenObjects); } },
+//				 { 'text': "Yes.", 'response': "Welcome home.", 'action': function() { loadHouse(onScreenObjects); } },
 //				 { 'text': "No.", 'response': "Let's do something first.", 'action': function() { } },
 //				 ]);
 	};
 	onScreenObjects.push(house);
-
-	var library = createProp(foreground, 1200, "library", 300, 0.63);
+	if(from && from == "house")
+		WORLD.GROUNDS.foreground.position.x = WORLD.WIDTH / 2 - house.getX();
+	
+	var library = createProp(WORLD.GROUNDS.foreground, 1200, "library", 300, 0.63);
 	library.interact = function() {
 		dialog("[the book has been returned]");
 		useItem("book", 0.15, 0.5);
 	};
 	onScreenObjects.push(library);
 	
-	var talker = createActor(foreground, 470);
+	var talker = createActor(WORLD.GROUNDS.foreground, 470);
 	talker.interact = function() {
 		dialog("How are you feeling today?",
 				[
@@ -30,35 +34,46 @@ function loadMainstreet(background, foreground, onScreenObjects) {
 	};
 	onScreenObjects.push(talker);
 	
-	var libquest = createActor(foreground, 100);
+	var libquest = createActor(WORLD.GROUNDS.foreground, 100);
 	libquest.interact = function() {
 		dialog("Hey, Square, you're heading towards the library, right? Can you return this book for me?",
 				[
-				 { 'text': "Sure.", 'response': "Thanks! [Hands Square the book]", 'action': function() { increaseMood(0.05, 0.3); aquireProp(createProp(foreground, 0, "book")); } },
+				 { 'text': "Sure.", 'response': "Thanks! [Hands Square the book]", 'action': function() { increaseMood(0.05, 0.3); aquireProp(createProp(WORLD.GROUNDS.foreground, 0, "book")); } },
 				 { 'text': "Okay, I guess.", 'response': "Oh, okay then.", 'action': function() { increaseMood(-0.05, 0.3); } },
 				 ]);
 	};
 	onScreenObjects.push(libquest);
+	
+	// create the ground
+	var ground = new PIXI.Sprite(getTexture("ground"));
+	ground.width = WORLD.WIDTH;
+	ground.position.y = WORLD.FLOOR;
+	WORLD.GROUNDS.staticforeground.addChild(ground);
+	onScreenObjects.push({'stage': {'item': ground } });
 }
 
-function loadHouse(background, foreground, onScreenObjects) {
-	emptyScreenObjects(background, foreground, onScreenObjects);
+
+function loadHouse(onScreenObjects) {
+	emptyScreenObjects(onScreenObjects);
+	WORLD.MOVEMENT = "player";
 	
-	var bedroom = createProp(foreground, 300, "bedroom", 400, 0.44);
-	bedroom.setY(bedroom.getY()+50);
+	var bedroom = createProp(WORLD.GROUNDS.staticforeground, WORLD.WIDTH * 0.44, "bedroom", WORLD.HEIGHT * 1.2, 0.44);
+	bedroom.setY(WORLD.HEIGHT);
+	bedroom.stage.item.width = WORLD.WIDTH;
 	bedroom.interact = function() {
-		loadMainstreet(background, foreground, onScreenObjects);
+		loadMainstreet(onScreenObjects, "house");
 //		dialog("Go outside?",
 //				[
-//				 { 'text': "Yes.", 'response': "Off you go.", 'action': function() { loadMainstreet(background, foreground, onScreenObjects); } },
+//				 { 'text': "Yes.", 'response': "Off you go.", 'action': function() { loadMainstreet(onScreenObjects); } },
 //				 { 'text': "No.", 'response': "Not right now.", 'action': function() { } },
 //				 ]);
 	};
 	onScreenObjects.push(bedroom);
+	WORLD.AGENT.setX(bedroom.getStageX());
 }
 
 
-function emptyScreenObjects(background, foreground, onScreenObjects) {
+function emptyScreenObjects(onScreenObjects) {
 	for(i in onScreenObjects)
 		removeObjectFromScene(onScreenObjects[i]);
 	onScreenObjects.length = 0;
