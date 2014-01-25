6ff27a6b0923c18fb478a9d7e8fb55e8d607d765
Garrett Tjarks <chicarksey@gmail.com>
2014-01-25 10:21:47 -0500

    when we load a new level, remove elements from the previous stage

--- src/main/pixi/js/levels.js
+++ src/main/pixi/js/levels.js
@@ -1,5 +1,21 @@
 function loadMainstreet(background, foreground, onScreenObjects) {
-	onScreenObjects.length = 0;
+	emptyScreenObjects(background, foreground, onScreenObjects);
 	onScreenObjects.push(createActor(background, 250));
 	onScreenObjects.push(createActor(foreground, 270));
+}
+
+function loadHouse() {
+	emptyScreenObjects(background, foreground, onScreenObjects);
+	onScreenObjects.push(createActor(background, 200));
+	onScreenObjects.push(createActor(foreground, 100));
+}
+
+
+function emptyScreenObjects(background, foreground, onScreenObjects) {
+	for(i in onScreenObjects)
+		for(j in onScreenObjects[i].stage) {
+			var sprite = onScreenObjects[i].stage[j];
+			sprite.parent.removeChild(sprite);
+		}
+	onScreenObjects.length = 0;
 }
